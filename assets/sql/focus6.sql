aa55711f49f11529aeefa5529146b085785fc963
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-11-15 10:13:54 +0100

    Database connection

---
+++ assets/sql/focus6.sql
@@ -0,0 +1,200 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 15, 2024 at 09:12 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `focus6`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactinfo`
+--
+
+CREATE TABLE `contactinfo` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logininfo`
+--
+
+CREATE TABLE `logininfo` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pagina's`
+--
+
+CREATE TABLE `pagina's` (
+  `id` int NOT NULL,
+  `paginaNaam` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paginagrid`
+--
+
+CREATE TABLE `paginagrid` (
+  `id` int NOT NULL,
+  `rowPosition` int NOT NULL,
+  `columnPosition` int NOT NULL,
+  `page` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paginainfo`
+--
+
+CREATE TABLE `paginainfo` (
+  `id` int NOT NULL,
+  `information` varchar(500) NOT NULL,
+  `position` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `socials`
+--
+
+CREATE TABLE `socials` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `link` varchar(500) NOT NULL,
+  `image` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contactinfo`
+--
+ALTER TABLE `contactinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logininfo`
+--
+ALTER TABLE `logininfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pagina's`
+--
+ALTER TABLE `pagina's`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paginagrid`
+--
+ALTER TABLE `paginagrid`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Constraint_FK_page_type` (`page`);
+
+--
+-- Indexes for table `paginainfo`
+--
+ALTER TABLE `paginainfo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Constraint_FK_position` (`position`);
+
+--
+-- Indexes for table `socials`
+--
+ALTER TABLE `socials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contactinfo`
+--
+ALTER TABLE `contactinfo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `logininfo`
+--
+ALTER TABLE `logininfo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pagina's`
+--
+ALTER TABLE `pagina's`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paginagrid`
+--
+ALTER TABLE `paginagrid`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paginainfo`
+--
+ALTER TABLE `paginainfo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `socials`
+--
+ALTER TABLE `socials`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `paginagrid`
+--
+ALTER TABLE `paginagrid`
+  ADD CONSTRAINT `Constraint_FK_page_type` FOREIGN KEY (`page`) REFERENCES `pagina's` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `paginainfo`
+--
+ALTER TABLE `paginainfo`
+  ADD CONSTRAINT `Constraint_FK_position` FOREIGN KEY (`position`) REFERENCES `paginagrid` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- core/db_connect.php
+++ core/db_connect.php
@@ -9,7 +9,7 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 
 
 $dbhost = "localhost";
-$dbname = "?";
+$dbname = "focus6";
 
 $con = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
 
