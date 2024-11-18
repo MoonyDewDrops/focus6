47b10eee41854a7bb4029052072203b8bd0a9892
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-11-18 15:39:03 +0100

    adminStart

--- views/login.php
+++ views/login.php
@@ -13,8 +13,6 @@
 
 
             <?php
-
-            include_once 'core/db_connect.php';
             if (isset($_POST['submit'])) {
                 //getting wachtwoord & user
                 $gebruikersnaam = mysqli_real_escape_string($con, $_POST['gebruikersnaam']);
@@ -32,8 +30,8 @@
                     $hash_pass = $row['wachtwoord'];
 
                     if (password_verify($wachtwoord, $hash_pass)) {
-                        $_SESSION['Savedgebruikersnaam'] = $gebruikersnaam;
-                        header("Location: admin.php");
+                        $_SESSION['gebruikersnaam'] = $gebruikersnaam;
+                        header("Location: admin");
                         exit();
                     } else {
             ?>
