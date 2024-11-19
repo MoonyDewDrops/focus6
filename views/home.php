db857f8f4ded5c9b8da7e9d92fc86910ea4f8ca3
PebblesDV <pebblesdevries@gmail.com>
2024-11-19 15:50:53 +0100

    pull

--- views/home.php
+++ views/home.php
@@ -10,35 +10,29 @@
 <body>
   <?php
   $sql = "SELECT id, naam, email, bericht FROM contactinfo;";
+  include_once "../core/db_connect.php";
+  // include "core/header.php";
+
+  $qry = $con->prepare($sql);
+  if ($qry === false) {
+    echo mysqli_error($con);
+  } else {
+    if ($qry->execute()) {
+      $qry->bind_result($id, $name, $email, $message);
+      while ($qry->fetch()) {
   ?>
-
-
-  <body>
-    <?php
-    $sql = "SELECT id, naam, email, bericht FROM contactinfo;";
-    include_once "core/db_connect.php";
-    // include "core/header.php";
-
-    $qry = $con->prepare($sql);
-    if ($qry === false) {
-      echo mysqli_error($con);
-    } else {
-      if ($qry->execute()) {
-        $qry->bind_result($id, $name, $email, $message);
-        while ($qry->fetch()) {
-    ?>
-          <div class="container">
-            <p>Dit is een test, verwijder als u begint met werken.</p>
-            <p>Naam: <?= $name ?></p>
-            <p>Email: <?= $email ?></p>
-            <p>Bericht: <?= $message ?></p>
-          </div>
-    <?php
-        }
+        <div class="container">
+          <p>Dit is een test, verwijder als u begint met werken.</p>
+          <p>Naam: <?= $name ?></p>
+          <p>Email: <?= $email ?></p>
+          <p>Bericht: <?= $message ?></p>
+        </div>
+  <?php
       }
-      $qry->close();
     }
-    ?>
-  </body>
+    $qry->close();
+  }
+  ?>
+</body>
 
 </html>
