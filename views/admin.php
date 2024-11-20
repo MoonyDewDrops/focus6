32b853a83bd211639e0e3065377e687eb55adb31
LGuijt <mail.lisageurts@gmail.com>
2024-11-20 09:22:26 +0100

    cms header done (for now)

--- views/admin.php
+++ views/admin.php
@@ -15,52 +15,35 @@ if (isset($_SESSION['gebruikersnaam'])) {
     }
 
     $con->close();
-?>
+    ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="<?= 'assets/css/style.css' ?>">
-    <link rel="stylesheet" href="<?= 'assets/css/admin.css' ?>">
-        <title>CMS</title>
-    </head>
-
-    <body>
-        <header>
-            <img></img>
-            <p>Welkom <?= $_SESSION['gebruikersnaam']; ?></p>
-            <a href="paginas">Paginas</a>
-            <a href="Socials">Socials</a>
-            <a href="contactberichten">Contactberichten</a>
-        </header>
-        <?php
-        if (isset($paginas)) {
-            foreach ($paginas as $pagina) {
-        ?>
-                <div>
-                    <p><?= $pagina['paginaNaam']; ?></p>
-                    <a href='editProcess?id=<?= $pagina['id'];?>' style="text-decoration:none;color:black;">Edit</a>
-                    <a href='deleteProcess?id=<?= $pagina['id'];?>' style="text-decoration:none;color:black;">Delete</a>
-                </div>
-        <?php
+    <?php include 'core/admin_header.php'; ?>
+        <div class="container">
+            <?php
+            if (isset($paginas)) {
+                foreach ($paginas as $pagina) {
+                    ?>
+                    <div>
+                        <p><?= $pagina['paginaNaam']; ?></p>
+                        <a href='editProcess?id=<?= $pagina['id']; ?>' style="text-decoration:none;color:black;">Edit</a>
+                        <a href='deleteProcess?id=<?= $pagina['id']; ?>' style="text-decoration:none;color:black;">Delete</a>
+                    </div>
+                    <?php
+                }
             }
-        }
-        ?>
-        
-        <a href="create" style="text-decoration:none;color:black;"> Pagina toevoegen </a>
-        <a href="contact" style="text-decoration:none;color:black;">Contacten bekijken</a>
-    <?php
+            ?>
+            <a href="create" style="text-decoration:none;color:black;"> Pagina toevoegen </a>
+            <a href="contact" style="text-decoration:none;color:black;">Contacten bekijken</a>
+        </div>
+        <?php
 } else if (!isset($_SESSION['gebruikersnaam'])) {
     ?>
-        <script>
-            location.replace("http://localhost/focus6/login");
-        </script>
-    <?php
+            <script>
+                location.replace("http://localhost/focus6/login");
+            </script>
+        <?php
 }
-    ?>
-    </body>
+?>
+</body>
 
-    </html>
+</html>
