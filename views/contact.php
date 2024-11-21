0c58647c2e02cef7959fdb0bbe1cbe5902cc9cf9
Dalysha <Dalyshajonis@outlook.com>
2024-11-21 13:52:15 +0100

    start

---
+++ views/contact.php
@@ -0,0 +1,36 @@
+<?php include "core/header.php"?>
+<div class="contact_box">
+ <div class="h1_contact">
+    <h1>Contact</h1>
+    <h3>Neem contact met ons op!</h3>
+    <form action="/action_page.php">
+
+  <label for="Vnaam">Naam:</label><br>
+  <input type="text" id="Vnaam" name="Vnaam" value="Volledige Naam"><br>
+  <label for="email">Emailadres:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <input type="submit" value="Submit">
+
+</form> 
+ </div>
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "core/footer.php" ?>
