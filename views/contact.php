bbd40e6761e93b128400e44752170ecf73bbc6d5
Dalysha <Dalyshajonis@outlook.com>
2024-11-21 14:45:20 +0100

    database connecten

--- views/contact.php
+++ views/contact.php
@@ -1,19 +1,24 @@
 <?php include "core/header.php"?>
-<div class="contact_box">
- <div class="h1_contact">
+
+<div class="contact_grid">
+
+ <div class="contact_tekst">
     <h1>Contact</h1>
     <h3>Neem contact met ons op!</h3>
-    <form action="/action_page.php">
-
-  <label for="Vnaam">Naam:</label><br>
-  <input type="text" id="Vnaam" name="Vnaam" value="Volledige Naam"><br>
+ </div>
+    
+<form class="contact_form" action="connect.php" method="post"> 
+  <!-- <form action="/action_page.php" target="_blank"></form> -->
+  <label for="naam">Naam:</label><br>
+  <input type="text" id="naam" name="Vnaam" value="Volledige Naam"><br>
   <label for="email">Emailadres:</label><br>
-  <input type="text" id="lname" name="lname" value=""><br><br>
-  <input type="submit" value="Submit">
+  <input type="text" id="email" name="email" value=""><br><br>
+  <label for="bericht">Bericht:</label><br>
+  <input type="text" id="bericht" name="bericht" value="...."><br>
 
+  <input type="submit" value="Submit" target=" ">
 </form> 
- </div>
- </div>
+</div>
 
 
 
