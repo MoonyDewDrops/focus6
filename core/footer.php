13538759a62d89d1d37c517695d0022e4d6e615b
PebblesDV <pebblesdevries@gmail.com>
2024-11-25 12:43:59 +0100

    footer

--- core/footer.php
+++ core/footer.php
@@ -8,18 +8,20 @@
 <body>
     <footer class="footer">
         <nav class="footer-top">
-            <a class="footer-link" href="">
+            <a class="footer-link" href="mailto: info@focus6.nl">
                 <img class="link-logo" src="assets/images/mail.svg" alt="mail logo">
                 <p class="link-text">info@focus6.nl</p>
             </a>
 
-            <img src="" alt="">
+            <img class="footer-logo" src="assets/images/logo.svg" alt="focus 6 logo">
 
-            <a class="footer-link" href="">
+            <a class="footer-link" href="tel: 0621937222">
                 <img class="link-logo" src="assets/images/phone.svg" alt="phone logo">
                 <p class="link-text">06-21937222</p>
             </a>
         </nav>
+
+        <p class="footer-bottom">Copyright © Alle rechten voorbehouden</p>
     </footer>
 </body>
 
