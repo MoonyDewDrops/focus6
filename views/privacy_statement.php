c994e0766fbbe7e1c2a2996f75c56332674df097
Dalysha <Dalyshajonis@outlook.com>
2024-12-05 21:57:04 +0100

    privacy statement klaar + button gestijlt

--- views/privacy_statement.php
+++ views/privacy_statement.php
@@ -1,13 +1,16 @@
-<p>Ingangsdatum: [4/12/2024]
-[Focus6] wij hechte grote waarde aan uw privacy en zet zich in om uw persoonsgegevens te beschermen. Dit privacybeleid legt uit hoe wij gegevens verzamelen, gebruiken, opslaan en verwijderen wanneer deze niet langer nodig zijn.
 
-1. Welke gegevens verzamelen wij?
-Wij kunnen de volgende soorten persoonsgegevens verzamelen wanneer u onze website bezoekt of onze diensten gebruikt:
+    
+<h2 class="h2_privacy" >Ingangsdatum: [4/12/2024]</h2>
+<h2>[Focus6] wij hechte grote waarde aan uw privacy en zet zich in om uw persoonsgegevens te beschermen. Dit privacybeleid legt uit hoe wij gegevens verzamelen, gebruiken, opslaan en verwijderen wanneer deze niet langer nodig zijn.</h2>
+
+<h3>1. Welke gegevens verzamelen wij?</h3>
+<h4>Wij kunnen de volgende soorten persoonsgegevens verzamelen wanneer u onze website bezoekt of onze diensten gebruikt:
 
 Contactgegevens: zoals uw naam, e-mailadres en het bericht dat u invoert.
 Formuliergegevens: informatie die u invult via onze contact- of registratieformulieren.
-Technische gegevens: zoals IP-adressen, browsergegevens, en cookies (voor analytische doeleinden).
-2. Hoe gebruiken wij uw gegevens?
+Technische gegevens: zoals IP-adressen, browsergegevens, en cookies (voor analytische doeleinden).</h4>
+
+<h3>2. Hoe gebruiken wij uw gegevens?</h3>
 
 Wij gebruiken uw gegevens voor de volgende doeleinden:
 Om contact met u op te nemen naar aanleiding van uw vragen of verzoeken.
@@ -15,11 +18,11 @@ Om onze diensten aan u te leveren of te verbeteren.
 Om wettelijke verplichtingen na te komen (zoals het bewaren van bepaalde gegevens voor administratieve doeleinden).
 Voor analytische doeleinden om onze website te verbeteren.
 
-3. Hoe bewaren wij uw gegevens?
+<h3>3. Hoe bewaren wij uw gegevens?</h3>
 Uw gegevens worden veilig opgeslagen in een beveiligde omgeving, beschermd door fysieke, technische en organisatorische maatregelen.
 Wij bewaren uw gegevens alleen zolang als nodig is om de doeleinden te vervullen waarvoor ze zijn verzameld, tenzij langere opslag vereist is door wet- of regelgeving.
 
-4. Wanneer worden uw gegevens verwijderd?
+<h3>4. Wanneer worden uw gegevens verwijderd?</h3>
 Persoonsgegevens worden verwijderd wanneer:
 
 Ze niet langer nodig zijn voor het oorspronkelijke doel waarvoor ze zijn verzameld.
@@ -28,13 +31,13 @@ Wij wettelijk verplicht zijn om gegevens te verwijderen.
 Hoe verwijderen wij gegevens?
 Gegevens worden op een veilige manier verwijderd of geanonimiseerd zodat deze niet langer herleidbaar zijn naar een individu.
 
-5. Delen wij uw gegevens?
+<h3>5. Delen wij uw gegevens?</h3>
 Wij delen uw gegevens alleen in de volgende gevallen:
 
 Met derde partijen die ons helpen bij het leveren van onze diensten (zoals hostingproviders), mits zij gebonden zijn aan een geheimhoudingsplicht.
 Wanneer dit wettelijk vereist is, bijvoorbeeld bij een juridische procedure of een gerechtelijk bevel.
 
-6. Uw rechten
+<h3>6. Uw rechten</h3>
 U heeft de volgende rechten met betrekking tot uw persoonsgegevens:
 
 Inzage: U mag weten welke gegevens wij van u hebben opgeslagen.
@@ -43,8 +46,9 @@ Verwijdering: U kunt verzoeken dat wij uw gegevens verwijderen als deze niet lan
 Bezwaar: U kunt bezwaar maken tegen het gebruik van uw gegevens voor specifieke doeleinden.
 Neem contact met ons op via [contactgegevens] om gebruik te maken van uw rechten.
 
-7. Cookies en tracking
+<h3>7. Cookies en tracking</h3>
 Onze website maakt gebruik van cookies om:
+
 
 De functionaliteit van de website te verbeteren.
 Anonieme statistieken te verzamelen over het gebruik van onze website.
@@ -52,7 +56,6 @@ U kunt cookies beheren via uw browserinstellingen. Raadpleeg ons [cookiebeleid] 
 
 
 
-Laatst bijgewerkt op: [4/12/2024]
+<h5> bijgewerkt op: [4/12/202]
 
-Met dit beleid voldoet u aan de privacywetgeving en zorgt u ervoor dat gebruikers begrijpen hoe hun gegevens worden behandeld.
-</p>
+Met dit beleid voldoet u aan de privacywetgeving en zorgt u ervoor dat gebruikers begrijpen hoe hun gegevens worden behandeld.</h5>
